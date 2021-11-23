6918580a8f76063ad223afb495cefb458b74b564
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2021-11-23 12:17:53 +0100

    Se han hecho cambios.

--- src/hello/Prueba.java
+++ src/hello/Prueba.java
@@ -3,7 +3,11 @@ package hello;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Adios mundo");
+		int[] ar = new int[10];
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+		}
+		System.out.println("Joe");
 	}
 
 }
